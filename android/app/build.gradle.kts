e9975b3d944052dd103ec12fdd8304141f9080de
shahriartoky0 <108423813+shahriartoky0@users.noreply.github.com>
2025-09-20 15:53:27 +0600

    changes: domain, add: minified, shrinkResource

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pass_rate"
+    namespace = "com.qemu.pass_rate"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pass_rate"
+        applicationId = "com.qemu.pass_rate"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -34,6 +34,8 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- android/app/src/main/kotlin/com/qemu/pass_rate/MainActivity.kt
+++ android/app/src/main/kotlin/com/qemu/pass_rate/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pass_rate
+package com.qemu.pass_rate
 
 import io.flutter.embedding.android.FlutterActivity
 
